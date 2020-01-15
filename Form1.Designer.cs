124b2d7cd05bafdaa3be3dafdfd4136a2707f066
Trevor Jackson <trevorj342@gmail.com>
2020-01-15 06:56:17 -0500

    All The Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MADCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numbersBox = new System.Windows.Forms.TextBox();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.meanBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numbersBox
+            // 
+            this.numbersBox.Location = new System.Drawing.Point(12, 296);
+            this.numbersBox.Multiline = true;
+            this.numbersBox.Name = "numbersBox";
+            this.numbersBox.Size = new System.Drawing.Size(269, 175);
+            this.numbersBox.TabIndex = 0;
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberLabel.Location = new System.Drawing.Point(10, 281);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(252, 12);
+            this.numberLabel.TabIndex = 1;
+            this.numberLabel.Text = "Numbers to make MAD! (seperated by commas, no spaces)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MAD:";
+            // 
+            // answerBox
+            // 
+            this.answerBox.Location = new System.Drawing.Point(15, 38);
+            this.answerBox.Multiline = true;
+            this.answerBox.Name = "answerBox";
+            this.answerBox.ReadOnly = true;
+            this.answerBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.answerBox.Size = new System.Drawing.Size(269, 22);
+            this.answerBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 167);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "MAKE MAD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mean:";
+            // 
+            // meanBox
+            // 
+            this.meanBox.Location = new System.Drawing.Point(15, 83);
+            this.meanBox.Multiline = true;
+            this.meanBox.Name = "meanBox";
+            this.meanBox.ReadOnly = true;
+            this.meanBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.meanBox.Size = new System.Drawing.Size(269, 22);
+            this.meanBox.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 483);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.meanBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.numbersBox);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "MAD Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numbersBox;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox meanBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace MADCalculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Regex regex = new Regex("^[a-zA-Z^$.|?*+(){}/`~_=';:><[]+$");
+            bool hasOnlyAlpha = regex.IsMatch(numbersBox.Text);
+            if (string.IsNullOrWhiteSpace(numbersBox.Text) || hasOnlyAlpha)
+            {
+                MessageBox.Show("I need numbers to do the math...", "Missing Numbers");
+                return;
+            }
+
+            string[] numbers = numbersBox.Text.Split(',');
+            List<int> ints = new List<int>() { };
+
+            foreach (string number in numbers)
+            {
+                int x = Int32.Parse(number);
+                ints.Add(x);
+            }
+
+            int sum = ints.Sum();
+            int mean = sum / ints.Count;
+            meanBox.Text = mean.ToString();
+
+            List<int> mads = new List<int>() { };
+            foreach (int number in ints)
+            {
+                int x = number - mean;
+                if (x < 0) { x *= -1; }
+                mads.Add(x);
+            }
+
+            float mad = (float)mads.Sum() / (float)mads.Count();
+            answerBox.Text = mad.ToString();
+        }
+    }
+}
